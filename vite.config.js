8c0a7f910306c626cf92924b4afdce234f9f8efd
Harikrishnan P T <harikrishnan.pt@digitalmesh.com>
2025-09-24 10:16:53 +0530

    Commit for vite change

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 import react from '@vitejs/plugin-react';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     build: {
@@ -46,6 +46,5 @@ export default defineConfig({
             ],
         }),
         react(),
-        reactRefresh()
     ],
 });
